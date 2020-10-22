030d9dad9d93aa1dce00c7c65f8a3dda9c220478
sk909923 <58472752+sk909923@users.noreply.github.com>
2020-10-22 15:13:25 +0530

    Update jenkins_plotly.py

--- jenkins_plotly.py
+++ jenkins_plotly.py
@@ -9,8 +9,8 @@ import plotly.express as px
 sc = SparkContext("local", "First App")
 sqlContext = SQLContext(sc)
 #requests.packages.urllib3.disable_warnings()
-df = sqlContext.read.format("com.databricks.spark.csv") 
-     \.options(header='true', inferschema='true').load("births.csv")
+df = sqlContext.read.format("com.databricks.spark.csv") \
+     .options(header='true', inferschema='true').load("births.csv")
 df.registerTempTable("births")
 df3= sqlContext.sql("SELECT * from births")
 df = px.data.iris()
