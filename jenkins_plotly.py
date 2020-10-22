8022686b72ff1a6ade91de0da27917c0863d52cd
sk909923 <58472752+sk909923@users.noreply.github.com>
2020-10-22 15:07:31 +0530

    Add files via upload

---
+++ jenkins_plotly.py
@@ -0,0 +1,17 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+import chart_studio.plotly as py
+import plotly.graph_objs as go
+import pandas as pd
+import requests
+import plotly.express as px
+
+sc = SparkContext("local", "First App")
+sqlContext = SQLContext(sc)
+#requests.packages.urllib3.disable_warnings()
+df = sqlContext.read.format("com.databricks.spark.csv") \.options(header='true', inferschema='true').load("births.csv")
+df.registerTempTable("births")
+df3= sqlContext.sql("SELECT * from births")
+df = px.data.iris()
+fig = px.scatter(pandf, x=df3.toPandas()['year'], y=df3.toPandas()['births'], color=df3.toPandas()['gender'])
+fig.show()
